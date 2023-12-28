645ba4d002e078ef93e2e40e4104a9d725683e3b
Ankit <ankitgupta88025@gmail.com>
2023-12-28 20:28:29 +0530

    update error

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config();
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>console.log(`server running at ${process.env.PORT}`))
+})
+.catch((err)=>{
+    console.log("mongo connection failed",err);
+})
