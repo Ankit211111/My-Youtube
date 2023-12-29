33af763334233157799626fae9140ac53a512562
Ankit <ankitgupta88025@gmail.com>
2023-12-29 14:28:49 +0530

    add user and video models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
 
 dotenv.config();
+const app =express();
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>console.log(`server running at ${process.env.PORT}`))
